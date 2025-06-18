56080f92aee6db3a494d61769e91f7c317506c09
Viacheslav Maximov <bmwmcsimov67@yandex.ru>
2025-06-18 20:14:47 +0300

    backup

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+
+from birthday.models import Birthday
+
+admin.site.empty_value_display = 'Не задано'
+
+@admin.register(Birthday)
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday',
+    )
